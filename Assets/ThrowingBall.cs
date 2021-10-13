72147507a9a76e214e4cfa9cc908b762e1a10809
Claudiu Popa <claudiupopa330@gmail.com>
2021-10-13 19:06:51 +0300

    Add Throwing ball logic

--- Assets/ThrowingBall.cs
+++ Assets/ThrowingBall.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-    
+using UnityEngine.XR.Interaction.Toolkit;
+
 public class ThrowingBall : MonoBehaviour
 {
 
-    public float throwingForce = 100f;
+    public float throwingForce = 1000f;
     public GameObject playerCamera;
     // Start is called before the first frame update
     void Start()
@@ -25,9 +26,13 @@ public class ThrowingBall : MonoBehaviour
         Debug.Log("TROW BALL");
 
         Rigidbody rb = this.gameObject.GetComponent<Rigidbody>();
-
+        XRGrabInteractable grab = GetComponent<XRGrabInteractable>();       
+        grab.interactionLayerMask = new LayerMask();
         rb.useGravity = true;
-        rb.AddForce( playerCamera.transform.forward * throwingForce);
+        rb.AddForce(playerCamera.transform.rotation * playerCamera.transform.forward * throwingForce);
+
+        // yield return new WaitForSeconds(5);
+        // grab.interactionLayerMask = 1;
     }
 
 }
